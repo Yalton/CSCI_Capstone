52079512a3f1aff7c12ed6d2b9afa2f9c009b8b8
yalt <yalt@Miranda>
2022-09-09 17:48:23 -0700

    GUI Work

--- src/themes.py
+++ src/themes.py
@@ -2,12 +2,12 @@
 themes = {}
 default = {}
 
-#Define default theme
+# Define default theme
 default['background_colo'] = "#321325"
 default['main_colo'] = "#5F0F40"
 default['accent_colo'] = "#9A031E"
 default['text_colo'] = "#CB793A"
 default['accent2_colo'] = "#FCDC4D"
 
-#Populate theme variable with theme set 
+# Populate theme variable with theme set
 themes['default'] = default
