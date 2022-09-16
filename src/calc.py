8c4a35e06a06a6f01c0af629bf9565f4dbd719e8
Yalton <73573710+Yalton@users.noreply.github.com>
2022-09-15 20:00:00 -0700

    exports added

---
+++ src/calc.py
@@ -0,0 +1,9 @@
+#Creation Date 9/7/22
+#Author: Dalton Bailey 
+#Course: CSCI 490
+#Instructor Sam Siewert
+
+import numpy as np
+import matplotlib as plot
+from os.path import exists
+import atexit
