1f1106fb67133e88641bdc344ac0a90d8da49aad
Your Name <you@example.com>
2022-09-18 10:41:24 -0700

    data structures started

--- src/calc.py
+++ src/calc.py
@@ -10,18 +10,52 @@ import sys
 import atexit
 import open3d as o3d
 
-# Read .ply file
-input_file = "input.ply"
-debug  = 1
-print(f"Program starting\n Attempting Open3d translation of .ply") if debug else print("")
-pcd = o3d.io.read_point_cloud(input_file) # Read the point cloud
-print("open3d point cloud read successfully") if debug else print("")
+class pholeCalc():
+    # Variables required by every function in the class
+    input_file = "data/input.ply"
+    debug  = 1
+    pcd = None
+    point_cloud_in_numpy = None
+    volume = None
+    density = None
+    mass = None
+    
+    #Init function
+    def __init__(self):
+        return 
+    
+    # Mesh visualization
+    def meshvis(self): 
+        # Visualize the point cloud within open3d
+        o3d.visualization.draw_geometries([self.pcd]) 
+        print("open3d visualization successful") if self.debug else print("")
+        return 
+    
+    # Mesh generation
+    def meshgen(self): 
+        print(f"Program starting\n Attempting Open3d translation of .ply") if self.debug else print("")
+        self.pcd = o3d.io.read_point_cloud(self.input_file) # Read the point cloud
+        print("open3d point cloud read successfully") if self.debug else print("")
+        self.meshvis() if self.debug else print("")
+        
+        # Convert open3d format to numpy array
+        # Here, you have the point cloud in numpy format. 
+        self.point_cloud_in_numpy = np.asarray(self.pcd.points) 
+        print("open3d point cloud read into numpy successfully") if self.debug else print("")
+        return 
 
-# Visualize the point cloud within open3d
-o3d.visualization.draw_geometries([pcd]) 
-print("open3d visualization successful") if debug else print("")
+    # Reference plane estimation 
+    def refest(self): 
+        return 
 
-# Convert open3d format to numpy array
-# Here, you have the point cloud in numpy format. 
-point_cloud_in_numpy = np.asarray(pcd.points) 
-print("open3d point cloud read into numpy successfully") if debug else print("")
+    # Volume calculation
+    def volcalc(self): 
+        return 
+
+    # Mass calculation
+    def masscalc(self): 
+        return 
+
+if __name__ == "__main__":
+    calc = pholeCalc()
+    calc.meshgen()
