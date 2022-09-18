7f2286f5e11cf513fcf258dbe060d7ab8e8f7b58
yalt <yalt@Miranda>
2022-09-17 19:44:43 -0700

    Numpy array translation successful

--- src/calc.py
+++ src/calc.py
@@ -11,17 +11,17 @@ import atexit
 import open3d as o3d
 
 # Read .ply file
-input_file = "input.ply"
+input_file = "data/input.ply"
 debug  = 1
-print(f"Program starting\n Attempting Open3d translation of .ply") if debug else print("")
+print(f"Program starting\n Attempting Open3d translation of .ply") if  debug  else print("")
 pcd = o3d.io.read_point_cloud(input_file) # Read the point cloud
-print("open3d point cloud read successfully") if debug else print("")
+print("open3d point cloud read successfully") if debug  else print("")
 
 # Visualize the point cloud within open3d
 o3d.visualization.draw_geometries([pcd]) 
-print("open3d visualization successful") if debug else print("")
+print("open3d visualization successful") if  debug  else print("")
 
 # Convert open3d format to numpy array
 # Here, you have the point cloud in numpy format. 
 point_cloud_in_numpy = np.asarray(pcd.points) 
-print("open3d point cloud read into numpy successfully") if debug else print("")
+print("open3d point cloud read into numpy successfully") if  debug  else print("")
